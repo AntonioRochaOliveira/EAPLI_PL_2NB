dbb616b4b10103aa1573e41d72924b9191411347
Joel <joel@joel-Notebook-PC-Ubuntu.(none)>
2013-04-05 17:24:28 +0100

    Implementação das classes para registo dos rendimentos.

---
+++ eapli.expensemanager/src/Controllers/RegisterIncomeController.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controllers;
+
+import Model.Income;
+import Model.IncomeType;
+import Persistence.IncomeRepository;
+import Persistence.IncomeTypeRepository;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author Joel Ferreira
+ */
+public class RegisterIncomeController {
+
+    IncomeTypeRepository incomeTypeRepository;
+    
+    public RegisterIncomeController() {
+    }
+    
+    public List getIncomesType(){
+        return incomeTypeRepository.getIncomeType();
+    }
+    
+    public void createIncome(BigDecimal amount, IncomeType incomeType, String what, Date date) {
+        Income income = new Income(amount, incomeType, what, date);
+        IncomeRepository repo = new IncomeRepository();
+        repo.save(income);
+    }
+}
