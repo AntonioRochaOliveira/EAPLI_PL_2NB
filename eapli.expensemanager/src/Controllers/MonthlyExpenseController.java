c774fd4ba832fab986bcc48159c0f751e31fe954
Mak3r <Mak3r@Mak3r-PC>
2013-04-11 21:57:40 +0100

    Adiçao controller de despesas mensais

---
+++ eapli.expensemanager/src/Controllers/MonthlyExpenseController.java
@@ -0,0 +1,34 @@
+package Controllers;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import Model.Expense;
+
+import java.util.Date;
+import Persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ *
+ * @author Mak3r
+ */
+public class MonthlyExpenseController {
+    
+    public MonthlyExpenseController(){
+        
+    }
+    
+    public void expensesList(int mes){
+        
+        List<Expense> expenses = new ArrayList<Expense>();
+        ExpenseRepository repo = new ExpenseRepository();
+        //expenses.getArrayList(repo); criar get da lista de despesas
+        //ValuesCalculator b = new ValuesCalculator();
+        //b.groupByMonth(expenses, mes);//conta corrente vai tratar do filtro de despesas por mes
+    }
+    
+}
