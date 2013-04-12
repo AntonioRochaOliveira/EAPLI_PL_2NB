82a91280e8ce8160a06d42dd4d2b8c5c508ba5cb
Joel <joel@joel-Notebook-PC-Ubuntu.(none)>
2013-04-12 18:40:25 +0100

    Implementação de interface IIncomeRepository

---
+++ eapli.expensemanager/src/Persistence/IIncomeRepository.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Persistence;
+import Model.*;
+
+/**
+ *
+ * @author joel
+ */
+public interface IIncomeRepository {
+    void save(Income income);
+}
