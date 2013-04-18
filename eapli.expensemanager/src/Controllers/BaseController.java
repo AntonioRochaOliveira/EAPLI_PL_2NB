10edc18f806793f8ec69d3f02919c9ae996fd2de
Luís Teixeira <teixeira13@gmail.com>
2013-04-18 23:08:23 +0100

    Retirei erros do git

--- eapli.expensemanager/src/Controllers/BaseController.java
+++ eapli.expensemanager/src/Controllers/BaseController.java
@@ -11,18 +11,11 @@ public abstract class BaseController {
 	public BigDecimal getBalance() {
 		
 		return buildCheckingAccount().getBalance();
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 	}
 	
 	public BigDecimal getWeeklyExpense(){
 		return buildCheckingAccount().getWeeklyExpense();
-=======
-		
->>>>>>> branch 'master' of git@github.com:AntonioRochaOliveira/EAPLI_PL_2NB.git
-=======
-		
->>>>>>> branch 'master' of git@github.com:AntonioRochaOliveira/EAPLI_PL_2NB.git
 	}
 
 }
