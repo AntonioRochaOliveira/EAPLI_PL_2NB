4a310fc4024734ffb7fdd43ac3bec6c492f64d03
Luís Teixeira <teixeira13@gmail.com>
2013-04-18 22:50:06 +0100

    Nova implementação do BaseUI para mostrar automaticamente um header e um footer

--- eapli.expensemanager/src/Presentation/BaseUI.java
+++ eapli.expensemanager/src/Presentation/BaseUI.java
@@ -4,12 +4,39 @@ import Controllers.BaseController;
 
 public abstract class BaseUI {
 	
-	public BaseUI(){
+//	public BaseUI(){
+//	}
+//
+//	public void displayBalance(){
+//		System.out.printf("* * *  CURRENT BALANCE  %.2f * * *\n",buildBaseController().getBalance());
+//	}
+//	
+//	public abstract BaseController buildBaseController();
+	
+	public void show(){
+		showHeader();
+		showContent();
+		showFooter();
 	}
-
-	public void displayBalance(){
+	
+	public void showHeader(){
+		System.out.print("* * * "+ getTitle() +" * * *\n");
+	}
+	
+	public void showFooter(){
 		System.out.printf("* * *  CURRENT BALANCE  %.2f * * *\n",buildBaseController().getBalance());
 	}
 	
+	
+	public String getTitle(){
+		//In implementation return null for default Title
+		return this.getClass().getSimpleName();
+	}
+	
+	//Implement your UI here
+	public abstract void showContent();
+	
+	//Return your controller 
 	public abstract BaseController buildBaseController();
+	
 }
