6535908797178ec5c869fbf1577f0ddab08a1489
Karlos <Karlos@Karlos-NoteBook>
2013-05-17 20:27:27 +0100

    return lista

--- eapli.expensemanager/src/Model/Income.java
+++ eapli.expensemanager/src/Model/Income.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 
