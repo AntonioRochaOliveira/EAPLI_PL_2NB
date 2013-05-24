80f02a6c820859bfeea4a9aca575a0de93169ef8
Daniel <Daniel@daniel-laptop>
2013-05-24 19:14:38 +0100

    Adicionado outro print.

--- eapli.expensemanager/test/Controllers/StartingBalanceControllerTest.java
+++ eapli.expensemanager/test/Controllers/StartingBalanceControllerTest.java
@@ -22,36 +22,5 @@ public class StartingBalanceControllerTest {
     public StartingBalanceControllerTest() {
     }
     
-    @BeforeClass
-    public static void setUpClass() {
-    }
     
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
-    @Test
-    public void testSetValue() {
-        System.out.println("setValue Test");
-        BigDecimal amount = new BigDecimal(0);
-        StartingBalanceController instance = new StartingBalanceController();
-        instance.setValue(amount);
-        assertEquals(amount, instance.getValue());
-    }
-
-    @Test
-    public void testGetValue() {
-        System.out.println("getValue Test");
-        StartingBalanceController instance = new StartingBalanceController();
-        if(instance.getValue()==null)
-            fail("The test case is a prototype.");
-    }
 }
--- eapli.expensemanager/test/Persistence/StartingBalanceRepositoryTest.java
+++ eapli.expensemanager/test/Persistence/StartingBalanceRepositoryTest.java
@@ -21,49 +21,5 @@ public class StartingBalanceRepositoryTest {
     public StartingBalanceRepositoryTest() {
     }
     
-    @BeforeClass
-    public static void setUpClass() {
-        BigDecimal expResult = new BigDecimal(0);
-        StartingBalanceRepository aux = StartingBalanceRepository.instance();
-        aux.setValue(expResult);
-    }
     
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
-    @Test
-    public void testInstance() {
-        System.out.println("instance Test");
-        BigDecimal expResult = StartingBalanceRepository.getValue();
-        StartingBalanceRepository result = StartingBalanceRepository.instance();
-        assertEquals(expResult, result.getValue());
-    }
-
-    @Test
-    public void testGetValue() {
-        System.out.println("getValue Test");
-        BigDecimal expResult = new BigDecimal(0);
-        StartingBalanceRepository aux = StartingBalanceRepository.instance();
-        aux.setValue(expResult);
-        BigDecimal result = StartingBalanceRepository.getValue();
-        assertEquals(expResult, result);
-    }
-
-    @Test
-    public void testSetValue() {
-        System.out.println("setValue Test");
-        BigDecimal value = new BigDecimal(100);
-        StartingBalanceRepository.setValue(value);
-        BigDecimal result = StartingBalanceRepository.getValue();
-        assertEquals(value, result);
-    }
 }
